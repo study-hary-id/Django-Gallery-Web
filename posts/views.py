e550bd2684f509e4c4385d03a1399ea1dafd542a
study-hary-id <haryansyah.study.id@gmail.com>
2021-03-15 20:59:57 +0700

    Edit views.py file Create new view for homepage Create new view for post new image

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from .forms import PostForm
+from .models import Post
 
-# Create your views here.
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
