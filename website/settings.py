e57b1cc7b25c41d2a5200bd42a5874ef931fc4ce
study-hary-id <haryansyah.study.id@gmail.com>
2021-03-16 19:40:15 +0700

    Add templates dir in root directory

--- website/settings.py
+++ website/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
