8f6353559cc9cca66f298d53bfeaaad77e28dbb4
study-hary-id <haryansyah.study.id@gmail.com>
2021-04-15 13:24:36 +0700

    Update url routes Delete forms and CreateView Add PostsConfig to the website app

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import HomePageView, CreatePostView
+from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('post', CreatePostView.as_view(), name='add_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,8 @@
-from django.views.generic import CreateView, ListView
-from django.urls import reverse_lazy
-from .forms import PostForm
+from django.views.generic import ListView
+
 from .models import Post
 
 
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
-
-
-class CreatePostView(CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'post.html'
-    success_url = reverse_lazy('home')
